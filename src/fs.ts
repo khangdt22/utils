920c396f2cb71a63c198752cb5ad48916a00bac6
Diep Dang <khangdt310722@gmail.com>
2023-08-12 18:52:01 +0700

    feat: add utils to read and write json file

--- src/fs.ts
+++ src/fs.ts
@@ -1,4 +1,5 @@
-import { accessSync, constants, type PathLike } from 'node:fs'
+import { accessSync, constants, type PathLike, readFileSync, existsSync } from 'node:fs'
+import JSON5 from 'json5'
 
 export function hasAccess(path: PathLike, mode?: number) {
     try {
@@ -20,4 +21,24 @@ export function isWritable(path: PathLike) {
 
 export function isReadableAndWritable(path: PathLike) {
     return hasAccess(path, constants.R_OK | constants.W_OK)
+}
+
+export function readJsonFile(path: PathLike, reviver?: Parameters<typeof JSON5.parse>[1]) {
+    if (!existsSync(path)) {
+        throw new Error(`File ${path} does not exist`)
+    }
+
+    if (!isReadable(path)) {
+        throw new Error(`File ${path} is not readable`)
+    }
+
+    return JSON5.parse(readFileSync(path, 'utf8'), reviver)
+}
+
+export function writeJsonFile(path: PathLike, data: any, options: Parameters<typeof JSON5.stringify>[1] = {}) {
+    if (!isWritable(path)) {
+        throw new Error(`File ${path} is not writable`)
+    }
+
+    return JSON5.stringify(data, options)
 }
