38b6ba2f0c3bdaf81a847a9b50e3c4e6882111c6
Diep Dang <khangdt310722@gmail.com>
2023-12-25 09:54:34 +0700

    chore: update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,36 @@
+import { parse, join } from 'node:path'
+import { existsSync } from 'node:fs'
+import { mkdir, cp, writeFile } from 'node:fs/promises'
+import fg from 'fast-glob'
 import { defineConfig } from 'tsup'
+import { readJsonFile } from './src/fs'
+import { omit } from './src/object'
 
 export default defineConfig({
     entry: ['src/*.ts'],
+    outDir: 'dist/lib',
     format: ['esm', 'cjs'],
     clean: true,
     shims: true,
     sourcemap: true,
-    dts: true,
+    dts: false,
+    onSuccess: async () => {
+        const files = fg.sync('src/**/*', { onlyFiles: true })
+
+        for (const file of files) {
+            const { dir, base } = parse(file.replace('src/', ''))
+            const outDir = join('dist', dir)
+
+            if (dir.length > 0 && !existsSync(dir)) {
+                await mkdir(outDir, { recursive: true })
+            }
+
+            await cp(file, join(outDir, base))
+        }
+
+        await writeFile(
+            'dist/package.json',
+            JSON.stringify(omit(await readJsonFile('package.json'), 'private'), null, 4)
+        )
+    },
 })
