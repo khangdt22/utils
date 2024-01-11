ba0e97dc6754222612753e853ce7edebb93dcab2
Diep Dang <khangdt310722@gmail.com>
2024-01-11 17:03:16 +0700

    feat: add dirname util

--- src/path.ts
+++ src/path.ts
@@ -1,4 +1,6 @@
 import type { PathLike } from 'node:fs'
+import { join, resolve, dirname as _dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { bufferToString } from './buffer'
 
 export function pathToString(path: PathLike) {
@@ -7,4 +9,8 @@ export function pathToString(path: PathLike) {
     }
 
     return path.toString()
+}
+
+export function dirname(importMeta: ImportMeta, ...path: PathLike[]) {
+    return join(resolve(_dirname(fileURLToPath(importMeta.url))), ...path.map(pathToString))
 }
