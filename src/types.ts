c69c8c57165b72002f51071b0ccb37b55c67c614
Diep Dang <khangdt310722@gmail.com>
2024-01-11 16:47:11 +0700

    feat: add some type utils

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
 export type Nullable<T> = T | null | undefined
 
 export type ContainsType<T, U> = Extract<T, U> extends never ? false : true
+
+export const toString = (value: unknown) => Object.prototype.toString.call(value)
+
+export function typeOf(v: unknown) {
+    if (v === null) {
+        return 'null'
+    }
+
+    return typeof v === 'object' || typeof v === 'function' ? toString(v).slice(8, -1).toLowerCase() : typeof v
+}
